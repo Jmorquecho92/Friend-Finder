1de41c844c6aa7c93018d88eac781ffd3f9461f5
Jmorquecho92 <morquecho.jesus@yahoo.com>
2017-11-24 13:24:46 -0800

    update files

---
+++ App/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+var path = require('path');
+//route
+module.exports = function(app){
+
+//GET request 
+	app.get('/survey', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+	});
+
+	// default to home
+	app.get('/',function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+
+}
