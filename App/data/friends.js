e8873ee62d9b6643e17f94c1d0af762837777953
Jmorquecho92 <morquecho.jesus@yahoo.com>
2017-11-24 12:46:31 -0800

    update files

---
+++ App/data/friends.js
@@ -0,0 +1,146 @@
+[
+  {
+    "name": "Frankie",
+    "photo": "http://www.shockmansion.com/wp-content/myimages/2016/03/rr231.jpg",
+    "location": "Inglewood, CA, USA",
+    "email": "myemail@gmail.com",
+    "scores": [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    "name": "Jasmine",
+    "photo": "https://www.google.com/search?biw=1373&bih=682&tbm=isch&sa=1&ei=HXoYWrLmDOjgjwSm_ILABw&q=jasmine&oq=jasmine&gs_l=psy-ab.3..0i67k1j0j0i67k1j0l3j0i67k1j0l3.26413.28750.0.28867.7.5.0.2.2.0.118.562.0j5.5.0....0...1c.1.64.psy-ab..0.7.573....0.9qdzti7vKnc#imgdii=w0zbBGkXyk-6EM:&imgrc=TBtlkDS89A7oOM:",
+    "location": "Inglewood, CA",
+    "email": "myemail@gmail.com",
+    "scores": [
+      "5",
+      "4",
+      "5",
+      "1",
+      "1",
+      "1",
+      "4",
+      "4",
+      "5",
+      "5"
+    ]
+  },
+  {
+    "name": "Natalie",
+    "photo": "https://vignette.wikia.nocookie.net/fairlyoddparents/images/c/ce/Patrick.gif/revision/latest?cb=20130305041931&path-prefix=en",
+    "location": "Inglewood, CA,",
+    "email": "myemail@gmail.com",
+    "scores": [
+      "2",
+      "3",
+      "5",
+      "4",
+      "3",
+      "2",
+      "1",
+      "5",
+      "3",
+      "4"
+    ]
+  },
+  {
+    "name": "Lyle",
+    "photo": "https://i.redd.it/5uyrc8opy9uy.jpg",
+    "location": "San Francisco, CA",
+    "email": "email@gmail.com",
+    "scores": [
+      "2",
+      "3",
+      "5",
+      "4",
+      "3",
+      "2",
+      "1",
+      "5",
+      "3",
+      "4"
+    ]
+  },
+  {
+    "name": "Tommy",
+    "photo": "https://vignette.wikia.nocookie.net/deadliestfiction/images/d/d2/Edward_01.jpg/revision/latest?cb=20120429072958",
+    "location": "La Mesa, CA",
+    "email": "email@gmail.com",
+    "scores": [
+      "3",
+      "2",
+      "3",
+      "3",
+      "5",
+      "3",
+      "5",
+      "2",
+      "5",
+      "3"
+    ]
+  },
+  {
+    "name": "Julian",
+    "photo": "https://nikkorpon.files.wordpress.com/2011/02/random-shots-059.jpg",
+    "location": "New York, NY",
+    "email": "email@gmail.com",
+    "scores": [
+      "3",
+      "2",
+      "3",
+      "3",
+      "5",
+      "3",
+      "5",
+      "2",
+      "5",
+      "3"
+    ]
+  },
+  {
+    "name": "Bobbie",
+    "photo": "https://pbs.twimg.com/profile_images/722399596224258049/ywBKj6en.jpg",
+    "location": "Spotswood, CA",
+    "email": "myemail@gmail.com",
+    "scores": [
+      "3",
+      "2",
+      "3",
+      "3",
+      "5",
+      "3",
+      "5",
+      "2",
+      "5",
+      "3"
+    ]
+  },
+  {
+    "name": "James",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/James_Dean_in_Rebel_Without_a_Cause.jpg/220px-James_Dean_in_Rebel_Without_a_Cause.jpg",
+    "location": "Inglewood, CA",
+    "email": "myemail@gmail.com",
+    "scores": [
+      "5",
+      "3",
+      "5",
+      "2",
+      "5",
+      "3",
+      "2",
+      "3",
+      "4",
+      "1"
+    ]
+  }
+]
