a53266a25f434608a0845b817913666f30eb1b2a
Jmorquecho92 <morquecho.jesus@yahoo.com>
2017-11-24 13:34:28 -0800

    update apiRoutes

---
+++ App/routing/apiRoutes.js
@@ -0,0 +1,52 @@
+
+var path = require('path');
+
+var friends = require('../data/friends.js');
+
+
+module.exports = function(app) {
+	
+
+	
+	app.get('/api/friends', function(req, res) {
+		res.json(friends);
+	});
+
+	// Add new friend entry
+	app.post('/api/friends', function(req, res) {
+		
+		var userInput = req.body;
+		
+
+		var userResponses = userInput.scores;
+
+		
+		var matchName = '';
+		var matchImage = '';
+		var totalDifference = 5000; 
+
+		
+		for (var i = 0; i < friends.length; i++) {
+			// console.log('friend = ' + JSON.stringify(friends[i]));
+
+			// Compute differenes for each question
+			var diff = 0;
+			for (var j = 0; j < userResponses.length; j++) {
+				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+			}
+			
+			if (diff < totalDifference) {
+
+				totalDifference = diff;
+				matchName = friends[i].name;
+				matchImage = friends[i].photo;
+			}
+		}
+
+		// Add new user
+		friends.push(userInput);
+
+		// Send appropriate response
+		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+	});
+};
